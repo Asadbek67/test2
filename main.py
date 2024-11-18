87b632a8bf1080c6b7989c7740df9d88249e47ce
Diyorbek008 <diyorbekmaxamadaliyev168@gmail.com>
2024-11-18 17:52:47 +0500

    qoshtim

--- main.py
+++ main.py
@@ -1,27 +1,58 @@
-# import psycopg2
+import psycopg2
 
-# conn = psycopg2.connect(
-#         host="localhost",
-#         user="postgres",
-#         password="907786789",
-#         dbname="test"
-#     )
-
-
-# cursor = conn.cursor()
-
-# cursor.execute('''
-#         CREATE TABLE IF NOT EXISTS students (
-#             student_id SERIAL PRIMARY KEY,
-#             name VARCHAR(100),
-#             age INTEGER,
-#             grade integer
-#         )
-#     ''')
+conn = psycopg2.connect(
+        host="localhost",
+        user="postgres",
+        password="907786789",
+        dbname="test"
+    )
 
 
-# cursor.execute("INSERT INTO students (name, age, grade) VALUES (%s, %s,%s)", ('Ali', 21, 85))
-# cursor.execute("INSERT INTO students (name, age, grade) VALUES (%s, %s, %s)", ('Vali', 22, 90))
-# cursor.execute("INSERT INTO students (name, age, grade) VALUES (%s, %s, %s)", ('Oleg', 20, 75))
+cursor = conn.cursor()
 
-# conn.commit()
+cursor.execute("""
+    DROP TABLE IF EXISTS students
+""")
+
+cursor.execute('''
+        CREATE TABLE IF NOT EXISTS students (
+            student_id SERIAL PRIMARY KEY,
+            name VARCHAR(100),
+            age INTEGER,
+            grade integer
+        )
+    ''')
+
+
+cursor.execute("INSERT INTO students (name, age, grade) VALUES (%s, %s,%s)", ('Ali', 21, 85))
+cursor.execute("INSERT INTO students (name, age, grade) VALUES (%s, %s, %s)", ('Vali', 22, 90))
+cursor.execute("INSERT INTO students (name, age, grade) VALUES (%s, %s, %s)", ('Oleg', 20, 75))
+
+# select
+cursor.execute("""
+    SELECT name, grade 
+    FROM students 
+    WHERE age > 21
+""")
+
+# Alining yoshini 90 ga yangilash
+cursor.execute("""
+    UPDATE students 
+    SET grade = %s 
+    WHERE name = %s
+""", (90, 'Ali'))
+
+# Reytingi 80 dan past bo'lgan talabalarni o'chirish
+cursor.execute("""
+    DELETE FROM students
+    WHERE grade < 80
+""")
+
+# Jadvalga 'email' ustunini qo'shish
+cursor.execute("""
+    ALTER TABLE students
+    ADD COLUMN email VARCHAR(100)
+""")
+
+
+conn.commit()
